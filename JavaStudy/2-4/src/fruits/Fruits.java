86953587a5422f931a8f46e74110869e7a676a4e
daifuku15 <daiki.f15@gmail.com>
2022-04-16 12:37:30 +0900

    Java課題2-4
    Java課題2-4

---
+++ JavaStudy/2-4/src/fruits/Fruits.java
@@ -0,0 +1,9 @@
+package fruits;
+
+public class Fruits {
+
+    public static void printFruits(String fruits, int count) {
+        System.out.println("H‚×•¨‚Í" + fruits);
+        System.out.println(count + "ŒÂ‚Å‚·");
+    }
+}
